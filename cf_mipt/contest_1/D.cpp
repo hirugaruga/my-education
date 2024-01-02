57df51e8969c27887fb9ba82d1d3d46b0d0513ee
odmiral <t.s.games@mail.ru>
2024-01-02 17:45:13 +0300

    Please enter the commit message for your changes. Lines starting just add some changes

--- cf_mipt/contest_1/D.cpp
+++ cf_mipt/contest_1/D.cpp
@@ -3,17 +3,67 @@
 //
 #include <iostream>
 
+template<typename T>
 class Queue {
  public:
-  void push(int val);
-  int size();
-  int pop();
-  bool empty();
+  Queue()
+  void enqueue(T val);
+  T dequeue();
+  void clear();
+  T front() const;
+  int size() const;
  private:
   struct Node {
-	int val;
+	T val;
+	Node* prev;
 	Node* next;
   };
+  Node* head_;
+  Node* tail_;
   int size_;
-  Node* prev_;
 };
+
+/****************************QUEUE*********************************************/
+
+template<typename T>
+Queue<T>::Queue() {
+  head_ = nullptr;
+  tail_ = nullptr;
+  size_ = 0;
+}
+
+template<typename T>
+void Queue<T>::enqueue(T val) {
+  Node* tmp = new Node;
+  tmp->val = val;
+  tmp->prev = nullptr;
+  tmp->next = nullptr;
+  if (size_ == 0) {
+	head_ = tmp;
+  } else {
+	tail_->next = tmp;
+  }
+  tmp->prev = tail_;
+  tail_ = tmp;
+  ++size_;
+}
+
+template <typename T>
+T Queue<T>::dequeue() {
+  if (size_ == 0) {
+	std::cerr << "Queue::dequeue : empty queue \n";
+  }
+  Node* tmp =head_;
+  if (size_ != 1){
+	head_ = head_->next;
+	head_ = prev = nullptr;
+  }
+  else{
+	head_ = nullptr;
+	tail_ = nullptr;
+  }
+  T val = tmp->val;
+  --size_;
+  delete tmp;
+  return val;
+}
